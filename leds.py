8c3aeace39ed42210bb52852c693177c7ebd18f2
Diego Robalino Bastidas <kira_diparoba3@hotmail.com>
2025-11-12 16:46:50 -0500

    Cree leds.py

---
+++ leds.py
@@ -0,0 +1,9 @@
+from machine import Pin
+import time
+
+led_uno = Pin(12, Pin.OUT)  # On-board LED for many ESP32 boards
+
+led_uno.on()   # Turn LED on
+time.sleep(1)  # Wait for 1 second
+led_uno.off()  # Turn LED off
+time.sleep(1)  # Wait for 1 second
